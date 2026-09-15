97d3bb353dbc8cec2aa17d88b78878fbf82ec579
fpidn <longntipc@gmail.com>
2026-09-15 15:33:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// Lưu trữ các thông tin cấu hình nhạy cảm hoặc cấu hình môi trường
+const supabaseUrl = 'https://umakbflruzbkfqokeftx.supabase.co';
+// WARNING: Điền Anon Key vào thay cho process.env.SUPABASE_KEY vì file HTML chạy trên trình duyệt client
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtYWtiZmxydXpia2Zxb2tlZnR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0NTMwODgsImV4cCI6MjEwNTAyOTA4OH0.FKbTixi6795i15oH_qgeZhnBtg1fj3hOzvHfTUPjgWw'
+
